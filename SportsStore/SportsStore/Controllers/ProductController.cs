4610a6658ab789d5776869ace2f59cac569170fd
lszlezak <l.szlezak@o2.pl>
2019-10-02 23:08:48 +0200

    Add project to repository.

---
+++ SportsStore/SportsStore/Controllers/ProductController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SportsStore.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SportsStore.Controllers
+{
+    public class ProductController : Controller
+    {
+        private readonly IProductRepository repository;
+
+        public ProductController(IProductRepository repository)
+        {
+            this.repository = repository;
+        }
+
+        public ViewResult List() => View(repository.Products);
+    }
+}
